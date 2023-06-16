b35343342842d49ec6cc573c766d98dac18dae5f
roudeina hamdi <75930493+roudeina812@users.noreply.github.com>
2023-06-16 14:38:52 +0100

    Update Project.php

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -4,6 +4,10 @@ namespace App\Entity;
 
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Serializer\Annotation\Ignore;
+use Doctrine\DBAL\Types\Types;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
 class Project
@@ -16,11 +20,33 @@ class Project
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $description = null;
 
     #[ORM\Transient]
     private ?string $photo;
+
+    #[ORM\OneToMany(mappedBy: 'project', targetEntity: ProjectPartners::class)]
+    private Collection $projectPartner;
+
+
+    #[ORM\Transient]
+    private Collection $partners;
+
+    /**
+     * @param string|null $title
+     * @param string|null $description
+     * @param string|null $type
+     */
+    public function __construct(?string $title, ?string $description, ?string $type)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->type= $type;
+    }
 
     public function getId(): ?int
     {
@@ -38,7 +64,21 @@ class Project
 
         return $this;
     }
+    /**
+     * @return string|null
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
 
+    /**
+     * @param string|null $type
+     */
+    public function setType(?string $type): void
+    {
+        $this->type = $type;
+    }
     public function getDescription(): ?string
     {
         return $this->description;
@@ -66,4 +106,53 @@ class Project
     {
         $this->photo = $photo;
     }
+
+    /**
+     * @param Collection $partners
+     */
+    public function setPartners(): void
+    {
+        $this->partners = new ArrayCollection();
+        for($i=0; $i<count($this->projectPartner); $i++)
+            $this->partners[$i]=$this->projectPartner[$i]->getPartner();
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getPartners(): Collection
+    {
+        return $this->partners;
+    }
+    /**
+     * @return Collection<int, ProjectPartners>
+     * @Ignore
+     */
+    public function getProjectPartner(): Collection
+    {
+        return $this->projectPartner;
+    }
+    public function addProjectPartner(ProjectPartners $projectPartner): self
+    {
+        if (!$this->projectPartner>contains($projectPartner)) {
+            $this->projectPartner->add($projectPartner);
+            $projectPartner->setProject($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProjectPartner(ProjectPartners $projectPartner): self
+    {
+        if ($this->projectPartner->removeElement($projectPartner)) {
+            // set the owning side to null (unless already changed)
+            if ($projectPartner->getProject() === $this) {
+                $projectPartner->setProject(null);
+            }
+        }
+
+        return $this;
+    }
+
+
 }
